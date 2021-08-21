05960869ef93173fe5dd3fc36e81f03c07faaccc
Vinayak Jaiwant <vjaiwantx@gmail.com>
2021-08-21 12:26:10 +0530

    Added new packages and changes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+//const favicon = require('serve-favicon');
+const path = require('path');
+
 const app = express();
 const port = process.env.port || 8000;
 const {v4:uuidv4} = require('uuid');
@@ -10,9 +13,12 @@ const guideRoutes = require('./routes/guides');
 const scholarRoutes = require('./routes/scholars');
 const adminRoutes = require('./routes/admin');
 
+
 app.set('view engine','pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
+app.use(express.static('public'))
+//aspp.use(express.favicon(path.join(__dirname, 'public','icons','favicon.ico')));
 app.use(cookieParser());
 app.use(morgan('combined'));
 app.use(session({
